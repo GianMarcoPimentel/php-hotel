3249ad04abd163a7184293ee0fc70a54ab8c5a1f
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-03-18 14:45:34 +0100

    Prima prova di ciclo foreach

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!-- 
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Iniziate in modo graduale.
+Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo, da bravi sviluppatori, aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+Bonus:
+1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+3 - Fare tutto in un'unica pagina: nella stessa pagina verrà mostrato l'elenco di tutti gli hotel e in base alla compilazione del form verrà mostrato al di sotto (dopo il refresh della pagina) l'elenco di hotel filtrato correttamente
+NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
+ -->
 <?php
 
     $hotels = [
@@ -50,5 +63,31 @@
 </head>
 <body>
     <h1>Hotels</h1>
+    <ul>
+
+        <?php
+        // var_dump($hotels);
+        foreach ($hotels as $currentHotel){
+            echo " 
+            <li>
+            " . $currentHotel['name'] . "
+            </li>
+            ";
+        }
+        ?>
+
+    </ul>
+    
 </body>
 </html>
+
+<!--  foreach ($hotels as $currentHotel){
+            echo "
+            <ul>
+                <li>
+                    " . $currentHotel .";
+                </li>
+                
+            </ul>
+            ";
+        }  -->
